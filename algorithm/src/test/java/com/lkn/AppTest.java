ed12f86ccc3131dd69ccf7a0600456f698ff3302
likangning <kangning.lkn@alibaba-inc.com>
2018-06-25 12:46:52 +0800

    完成B树的添加操作

---
+++ algorithm/src/test/java/com/lkn/AppTest.java
@@ -0,0 +1,19 @@
+package com.lkn;
+
+import com.google.common.collect.Lists;
+import org.junit.Test;
+
+import java.util.List;
+
+public class AppTest {
+
+	@Test
+	public void shouldAnswerWithTrue() {
+		System.out.println("1,2,3,4");
+		System.out.println("-");
+		System.out.println(" -");
+		System.out.println("  -");
+		System.out.println("   -");
+		System.out.println("    -");
+	}
+}
