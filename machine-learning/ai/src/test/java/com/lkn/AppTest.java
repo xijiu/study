eee1b29f11f8005035889138a51efc064be3cbe8
likangning <kangning.lkn@alibaba-inc.com>
2018-09-10 14:14:10 +0800

    神经网络学习

---
+++ machine-learning/ai/src/test/java/com/lkn/AppTest.java
@@ -0,0 +1,58 @@
+package com.lkn;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import com.lkn.ai.uct.Slot;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+	@Test
+	public void shouldAnswerWithTrue() {
+		List<Bean> set = Lists.newArrayList();
+		Bean bean = new Bean(3);
+		set.add(new Bean(9));
+		set.add(bean);
+		set.add(new Bean(5));
+		bean.setId(10);
+
+		Collections.sort(set);
+		System.out.println(set);
+	}
+
+	@Data
+	@AllArgsConstructor
+	private class Bean implements Comparable<Bean> {
+		private int id;
+
+		@Override
+		public int compareTo(Bean o) {
+			return Integer.compare(this.id, o.id);
+		}
+	}
+
+	@Test
+	public void test() {
+		List<Slot> list = Lists.newArrayList();
+		Slot slot1 = Slot.newInstance(1);
+		slot1.setPlayTimes(0);
+
+		Slot slot2 = Slot.newInstance(2);
+		slot2.setPlayTimes(1);
+
+		list.add(slot1);
+		list.add(slot2);
+		Collections.sort(list);
+		System.out.println(list);
+	}
+}
