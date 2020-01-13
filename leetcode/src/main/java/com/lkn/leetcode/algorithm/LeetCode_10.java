f8d9a399df13707f1fe56617360a1942e1cac88e
likangning <kangning.lkn@alibaba-inc.com>
2020-01-13 10:23:54 +0800

    力扣第10题，正则表达式解决

--- leetcode/src/main/java/com/lkn/leetcode/algorithm/LeetCode_10.java
+++ leetcode/src/main/java/com/lkn/leetcode/algorithm/LeetCode_10.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 /**
  * 正则表达式
+ * 采用了2种解决思路：递归、动态规划
+ * 结论：还是动态规划性能好，递归的最好成绩是15ms，动态规划最好成绩2ms
  *
  * 	mississippi
  *	mis*is*p*.
@@ -12,135 +14,171 @@ import org.junit.Test;
  */
 public class LeetCode_10 {
 
+//	@Test
+//	public void test() {
+//		Object result = new Solution().isMatch("mississippi", "mis*is*p*.");
+////		Object result = new Solution().isMatch("aa", "a");
+////		Object result = new Solution().isMatch("aa", "a*");
+////		Object result = new Solution().isMatch("a", "ab*");
+////		Object result = new Solution().isMatch("", "c*c*");
+//		System.out.println(result);
+//		System.out.println((int) '*');
+//		System.out.println((int) '.');
+//	}
+
+//	/**
+//	 * 递归解决方案
+//	 */
+//	class Solution {
+//
+//		private boolean isMatch = false;
+//		private char[] baseChars;
+//		private char[] exprChars;
+//		private int XING = 42;
+//		private int DOT = 46;
+//		public boolean isMatch(String s, String p) {
+//			isMatch = false;
+//			baseChars = s.toCharArray();
+//			exprChars = p.toCharArray();
+//			traverseMatch(0, 0);
+//			return isMatch;
+//		}
+//
+//		private void traverseMatch(int baseIndex, int exprIndex) {
+//			if (isMatch) {
+//				return;
+//			}
+//			if (baseIndex >= baseChars.length || exprIndex >= exprChars.length) {
+//				if (baseIndex == baseChars.length && exprIndex == exprChars.length) {
+//					isMatch = true;
+//				} else if (baseIndex == baseChars.length && (exprChars.length - exprIndex) % 2 == 0) {
+//					boolean flag = true;
+//					for (int i = exprIndex + 1; i < exprChars.length; i = i + 2) {
+//						if (exprChars[i] != XING) {
+//							flag = false;
+//						}
+//					}
+//					if (flag) {
+//						isMatch = true;
+//					}
+//				}
+//				return;
+//			}
+//			char baseChar = baseChars[baseIndex];
+//			char exprChar = exprChars[exprIndex];
+//			// 最后一个字符
+//			if (exprIndex == exprChars.length - 1) {
+//				if ((baseChar == exprChar || exprChar == DOT) && baseIndex == baseChars.length - 1) {
+//					isMatch = true;
+//				}
+//			} else {
+//				char nextExprChar = exprChars[exprIndex + 1];
+//				if (nextExprChar == XING) {
+//					// 认为*的匹配次数为0
+//					traverseMatch(baseIndex, exprIndex + 2);
+//					boolean isMatchAll = exprChar == DOT;
+//
+//					// 认为*的匹配次数为多个
+//					int matchNum = 1;
+//					while (!isMatch) {
+//						if (baseIndex + matchNum > baseChars.length) {
+//							break;
+//						}
+//						boolean isEqual = true;
+//						if (!isMatchAll) {
+//							for (int i = baseIndex; i < baseIndex + matchNum; i++) {
+//								if (baseChars[i] != exprChar) {
+//									isEqual = false;
+//									break;
+//								}
+//							}
+//						}
+//						if (isEqual) {
+//							traverseMatch( baseIndex + matchNum, exprIndex + 2);
+//						} else {
+//							break;
+//						}
+//						matchNum++;
+//					}
+//				} else {
+//					if (baseChar == exprChar || exprChar == DOT) {
+//						if (baseIndex == baseChars.length - 1 && exprIndex == exprChars.length - 1) {
+//							isMatch = true;
+//						} else {
+//							traverseMatch( baseIndex + 1, exprIndex + 1);
+//						}
+//					}
+//				}
+//			}
+//		}
+//	}
+
+	/**
+	 * 采用动态规划思路，用二维数组来记录状态
+	 */
 	@Test
-	public void test() {
-		Object result = new Solution().isMatch("mississippi", "mis*is*p*.");
-//		Object result = new Solution().isMatch("aa", "a");
+	public void test3() {
+		// aaa  ab*ac*a
+//		Object result = new Solution().isMatch("aaa", "ab*ac*a");
 //		Object result = new Solution().isMatch("aa", "a*");
-//		Object result = new Solution().isMatch("a", "ab*");
-//		Object result = new Solution().isMatch("", "c*c*");
-		System.out.println(result);
-		System.out.println((int) '*');
-		System.out.println((int) '.');
-	}
-
-	class Solution {
-
-		private boolean isMatch = false;
-		private char[] baseChars;
-		private char[] exprChars;
-		private int XING = 42;
-		private int DOT = 46;
-		public boolean isMatch(String s, String p) {
-			isMatch = false;
-			baseChars = s.toCharArray();
-			exprChars = p.toCharArray();
-			traverseMatch(0, 0);
-			return isMatch;
-		}
-
-		private void traverseMatch(int baseIndex, int exprIndex) {
-			if (isMatch) {
-				return;
-			}
-			if (baseIndex >= baseChars.length || exprIndex >= exprChars.length) {
-				if (baseIndex == baseChars.length && exprIndex == exprChars.length) {
-					isMatch = true;
-				} else if (baseIndex == baseChars.length && (exprChars.length - exprIndex) % 2 == 0) {
-					boolean flag = true;
-					for (int i = exprIndex + 1; i < exprChars.length; i = i + 2) {
-						if (exprChars[i] != XING) {
-							flag = false;
-						}
-					}
-					if (flag) {
-						isMatch = true;
-					}
-				}
-				return;
-			}
-			char baseChar = baseChars[baseIndex];
-			char exprChar = exprChars[exprIndex];
-			// 最后一个字符
-			if (exprIndex == exprChars.length - 1) {
-				if ((baseChar == exprChar || exprChar == DOT) && baseIndex == baseChars.length - 1) {
-					isMatch = true;
-				}
-			} else {
-				char nextExprChar = exprChars[exprIndex + 1];
-				if (nextExprChar == XING) {
-					// 认为*的匹配次数为0
-					traverseMatch(baseIndex, exprIndex + 2);
-					boolean isMatchAll = exprChar == DOT;
-
-					// 认为*的匹配次数为多个
-					int matchNum = 1;
-					while (!isMatch) {
-						if (baseIndex + matchNum > baseChars.length) {
-							break;
-						}
-						boolean isEqual = true;
-						if (!isMatchAll) {
-							for (int i = baseIndex; i < baseIndex + matchNum; i++) {
-								if (baseChars[i] != exprChar) {
-									isEqual = false;
-									break;
-								}
-							}
-						}
-						if (isEqual) {
-							traverseMatch( baseIndex + matchNum, exprIndex + 2);
-						} else {
-							break;
-						}
-						matchNum++;
-					}
-				} else {
-					if (baseChar == exprChar || exprChar == DOT) {
-						if (baseIndex == baseChars.length - 1 && exprIndex == exprChars.length - 1) {
-							isMatch = true;
-						} else {
-							traverseMatch( baseIndex + 1, exprIndex + 1);
-						}
-					}
-				}
-			}
-		}
-	}
-
-
-	@Test
-	public void test2() {
-		Object result = new Solution1().isMatch("abcdefg", ".*");
+		Object result = new Solution().isMatch("", ".*");
 		System.out.println("final result is : " + result);
 	}
 
-	class Solution1 {
+	class Solution {
 		public boolean isMatch(String s, String p) {
-			int sLen = s.length(), pLen = p.length();
-			boolean[][] memory = new boolean[sLen + 1][pLen + 1];
-			memory[0][0] = true;
-			for (int i = 0; i <= sLen; i++) {
-				for (int j = 1; j <= pLen; j++) {
-					if (p.charAt(j - 1) == '*') {
-						memory[i][j] = memory[i][j - 2] || (i > 0 && (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.') && memory[i - 1][j]);
-					} else {
-						memory[i][j] = i > 0 && (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.') && memory[i - 1][j - 1];
+			char[] baseArr = s.toCharArray();
+			char[] exprArr = p.toCharArray();
+			if (baseArr.length == 0) {
+				return emptyBaseMatch(exprArr);
+			} else if (exprArr.length == 0) {
+				return false;
+			}
+			boolean[][] result = new boolean[baseArr.length][exprArr.length];
+			// 首选处理第一行
+			int placeholderNum = 0;
+			for (int j = 0; j < exprArr.length; j++) {
+				if (baseArr[0] == exprArr[j] || exprArr[j] == '.') {
+					if (placeholderNum == 0) {
+						result[0][j] = true;
 					}
-					arrayToString(memory);
+				} else {
+					if (exprArr[j] == '*') {
+						result[0][j] = result[0][j - 1] || (j >= 2 && result[0][j - 2]);
+					}
+				}
+				if (exprArr[j] == '*') {
+					placeholderNum--;
+				} else {
+					placeholderNum++;
 				}
 			}
-			return memory[sLen][pLen];
+
+			for (int i = 1; i < baseArr.length; i++) {
+				for (int j = 0; j < exprArr.length; j++) {
+					if (exprArr[j] == '*') {
+						result[i][j] = result[i][j - 1] || result[i - 1][j] && (exprArr[j - 1] == baseArr[i] || baseArr[i] == '.' || exprArr[j - 1] == '.');
+						if (j >= 2 && !result[i][j]) {
+							result[i][j] = result[i][j - 2];
+						}
+					} else {
+						result[i][j] = j > 0 && result[i - 1][j - 1] && (baseArr[i] == exprArr[j] || exprArr[j] == '.');
+					}
+				}
+			}
+			return result[baseArr.length - 1][exprArr.length - 1];
 		}
 
-		private void arrayToString(boolean[][] memory) {
-			for (int i = 0; i < memory.length; i++) {
-				for (int j = 0; j < memory[i].length; j++) {
-					System.out.print(memory[i][j] + ", \t");
+		private boolean emptyBaseMatch(char[] exprArr) {
+			int placeholder = 0;
+			for (char c : exprArr) {
+				if (c == '*') {
+					placeholder--;
+				} else {
+					placeholder++;
 				}
-				System.out.println();
 			}
-			System.out.println("\r\n\n **********************************");
+			return placeholder == 0;
 		}
 	}
 }
